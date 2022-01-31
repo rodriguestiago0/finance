1418a785409929dfa65ca4deebed5cb83187f638
Tiago Rodrigues <tiagomcrodrigues@gmail.com>
2022-01-31 00:50:23 +0000

    code clean up

--- Finance.Service/Worker.fs
+++ Finance.Service/Worker.fs
@@ -8,14 +8,16 @@ open Microsoft.Extensions.Logging
 
 type WorkerService(_logger: ILogger<WorkerService>) =
     member val timer: Timer option = None with get, set
+    member val cancellationToken: CancellationToken option = None with get, set
 
     interface IHostedService with
-        member this.StartAsync(token: CancellationToken) =
+        member this.StartAsync(cancellationToken: CancellationToken) =
             "Worker Service running" |> _logger.LogInformation
             this.timer <- Some(new Timer(this.DoWork, null, TimeSpan.Zero, TimeSpan.FromSeconds(5.)))
+            this.cancellationToken <- Some cancellationToken
             Task.CompletedTask
 
-        member this.StopAsync(token: CancellationToken) =
+        member this.StopAsync(cancellationToken: CancellationToken) =
             "Worker Service is stopping" |> _logger.LogInformation
             match this.timer with
             | Some(t) -> t.Change(Timeout.Infinite, 0) |> ignore
