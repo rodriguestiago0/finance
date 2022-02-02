6ef5af0db784825f41daa50d29778be6d10c68cd
Tiago Rodrigues <tiagomcrodrigues@gmail.com>
2022-02-02 21:13:51 +0000

    fixed repository

--- Finance.Repository/TransactionsRepository.fs
+++ Finance.Repository/TransactionsRepository.fs
@@ -1,5 +1,6 @@
 ﻿namespace Finance.Repository
 
+open FSharpPlus
 open Finance.FSharp
 open Npgsql
 open Npgsql.FSharp
@@ -8,32 +9,35 @@ open Finance.Repository.Models
 
 module TransactionsRepository =
     let createTransactions connectionString (transactions : Transaction[]) =
-        use connection = new NpgsqlConnection(connectionString)
-        connection.Open()
-        
-        use transaction = connection.BeginTransaction()
-
-        let insert (transaction : TransactionDto) =
-            connection
-            |> Sql.existingConnection
-            |> Sql.query "INSERT INTO
-                    Transaction (TransactionId, ExternalTransactionId, TickerId, Date, Units, Price, LocalPrice, Fee, ExchangeRate)
-                    VALUES (@TransactionId, @ExternalTransactionId, @TickerId, @Date, @Units, @Price, @LocalPrice, @Fee, @ExchangeRate)"
-            |> Sql.parameters [ "@TransactionId", Sql.uuid transaction.TransactionId ]
-            |> Sql.parameters [ "@ExternalTransactionId", Sql.stringOrNone transaction.ExternalTransactionId ]
-            |> Sql.parameters [ "@TickerId", Sql.uuid transaction.TickerId ]
-            |> Sql.parameters [ "@Date", Sql.timestamptz transaction.Date ]
-            |> Sql.parameters [ "@Units", Sql.decimal transaction.Units ]
-            |> Sql.parameters [ "@Price", Sql.decimal transaction.Price ]
-            |> Sql.parameters [ "@LocalPrice", Sql.decimalOrNone transaction.LocalPrice ]
-            |> Sql.parameters [ "@Fee", Sql.decimal transaction.Fee ]
-            |> Sql.parameters [ "@ExchangeRate", Sql.decimalOrNone transaction.ExchangeRate ]
-            |> Sql.executeNonQuery
-            |> ignore
-        
-        transactions
-        |> Seq.map TransactionDto.ofDomain
-        |> Seq.iter insert
-        
-        transaction.Commit()
-        AsyncResult.retn ()
+        async {
+            
+            use connection = new NpgsqlConnection(connectionString)
+            do! connection.OpenAsync() |> Async.AwaitTask
+            
+            use transaction = connection.BeginTransaction()   
+            
+            let transactionDto =
+                transactions
+                |> Seq.map TransactionDto.ofDomain
+                
+            for transaction in transactionDto do
+                connection
+                |> Sql.existingConnection
+                |> Sql.query "INSERT INTO
+                        Transaction (TransactionId, ExternalTransactionId, TickerId, Date, Units, Price, LocalPrice, Fee, ExchangeRate)
+                        VALUES (@TransactionId, @ExternalTransactionId, @TickerId, @Date, @Units, @Price, @LocalPrice, @Fee, @ExchangeRate)"
+                |> Sql.parameters [ "@TransactionId", Sql.uuid transaction.TransactionId ]
+                |> Sql.parameters [ "@ExternalTransactionId", Sql.stringOrNone transaction.ExternalTransactionId ]
+                |> Sql.parameters [ "@TickerId", Sql.uuid transaction.TickerId ]
+                |> Sql.parameters [ "@Date", Sql.timestamptz transaction.Date ]
+                |> Sql.parameters [ "@Units", Sql.decimal transaction.Units ]
+                |> Sql.parameters [ "@Price", Sql.decimal transaction.Price ]
+                |> Sql.parameters [ "@LocalPrice", Sql.decimalOrNone transaction.LocalPrice ]
+                |> Sql.parameters [ "@Fee", Sql.decimal transaction.Fee ]
+                |> Sql.parameters [ "@ExchangeRate", Sql.decimalOrNone transaction.ExchangeRate ]
+                |> Sql.executeNonQueryAsync
+                |> AsyncResult.ofTask |> ignore
+                    
+            do! transaction.CommitAsync() |> Async.AwaitTask
+            return Ok()
+        }
